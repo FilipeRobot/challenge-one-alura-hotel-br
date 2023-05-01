de7509084d9ded4ed0fa0e195fa505b874409f5e
Filipe Lemos <filipe_lemos@live.com>
2023-05-01 16:05:26 -0300

    Atualizando README.md e JPAUtils

--- src/main/java/com/github/FilipeRobot/utils/JPAUtils.java
+++ src/main/java/com/github/FilipeRobot/utils/JPAUtils.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtils {
-    private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("db_hotel_alura");
+    private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory(System.getenv("UNIT_NAME"));
 
     public static EntityManager getEntityManager() {
         return FACTORY.createEntityManager();
